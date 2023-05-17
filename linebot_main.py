662fa412b95fa86f8c60fe8317c5566e3aea7f88
onoriyo <m2230080@photon.chitose.ac.jp>
2023-05-17 19:42:14 +0900

    Update linebot_main.py

--- linebot_main.py
+++ linebot_main.py
@@ -127,24 +127,19 @@ app = FastAPI()
 
 @app.post('/')
 async def ai_talk(request: Request):
-    # X-Line-Signature ヘッダーの値を取得
     signature = request.headers.get('X-Line-Signature', '')
 
-    # request body から event オブジェクトを取得
     events = line_parser.parse((await request.body()).decode('utf-8'), signature)
 
-    # 各イベントの処理（※1つの Webhook に複数の Webhook イベントオブジェクトが含まれる場合あるため）
     for event in events:
         if event.type != 'message':
             continue
         if event.message.type != 'text':
             continue
-
-        # LINE パラメータの取得
+            
         line_user_id = event.source.user_id
         line_message = event.message.text
 
-        # ChatGPT からトークデータを取得
         response = openai.ChatCompletion.create(
             model = 'gpt-3.5-turbo'
             , temperature = 0.0
@@ -156,8 +151,6 @@ async def ai_talk(request: Request):
         )
         ai_message = response.choices[0].message.content
 
-        # LINE メッセージの送信
         line_bot_api.push_message(line_user_id, TextSendMessage(ai_message))
 
-    # LINE Webhook サーバーへ HTTP レスポンスを返す
     return 'ok'
